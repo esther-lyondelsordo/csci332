fe2432891fe855dee7307c0275768083aab75d5f
Esther Lyon Delsordo <esther.lyondelsordo@gmail.com>
2022-10-31 13:22:26 -0600

    Build API

--- softwareAssignment1.py
+++ softwareAssignment1.py
@@ -6,6 +6,32 @@ Due November 14, 2022
 
 Implement Kruskal's algorithm to find the MST of a weighted graph
 """
+
+
 class KruskalMST:
-    # Write class here
-    
+    # initialize class values
+    def __init__(self, graph, mst):
+        self.name = graph
+        self.age = mst
+
+    # find MST from an adjacency matrix
+    def findMinimumSpanningTree(A):
+        # if A_ij > 0 there is an edge from node i to node j
+        # returns MST T, also in adjacency matrix form
+
+    # mergesort algorithm
+    def mergesort(a):
+        # Sort the values in array ‘a’ and return the sorted array ‘b’.
+
+    # build union-find data structure
+    def makeUnionFind(S):
+        # Convert a set ‘S’ into a union-find data structure F
+
+    # find function
+    def find(u):
+        # Return the connected component of the element ‘u’ in ‘S’
+
+    # union function
+    def union(A,B):
+        # Combine the connected components given by node sets ‘A’ and ‘B’ into set C
+
