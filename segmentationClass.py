b58b1d34fb8d2da2c3df408cee3e2f1b5b8c16ad
Esther Lyon Delsordo <esther.lyondelsordo@gmail.com>
2022-11-28 13:16:04 -0700

    Software Assignment 2 WIP

---
+++ segmentationClass.py
@@ -0,0 +1,11 @@
+"""
+CSCI 332
+Esther Lyon Delsordo
+Software Assignment #2
+Due December 10, 2022
+
+Implement Ford-Fulkerson Alg and use it to implement the image
+segmentation algorithm from Kelinberg-Tardos 7.10
+"""
+
+import numpy as np
---
+++ segmentationTest.py
@@ -0,0 +1,13 @@
+"""
+CSCI 332
+Esther Lyon Delsordo
+Software Assignment #2
+Due December 10, 2022
+
+Implement Ford-Fulkerson Alg and use it to implement the image
+segmentation algorithm from Kelinberg-Tardos 7.10
+
+Test script for segmentationClass.py
+"""
+
+import numpy as np
